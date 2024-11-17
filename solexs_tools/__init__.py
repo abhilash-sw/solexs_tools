04c041ed81352eec298a0fe8f3e5f01c4f4ac09f
Abhilash Sarwade <as.sarwade@gmail.com>
2024-11-17 22:20:45 +0530

    Test

--- solexs_tools/__init__.py
+++ solexs_tools/__init__.py
@@ -5,7 +5,7 @@
 # @File Name: __init__.py
 # @Project: solexs_tools
 #
-# @Last Modified time: 2024-11-17 10:09:32 pm
+# @Last Modified time: 2024-11-17 10:20:38 pm
 #####################################################
 
 __version__ = 0.1
@@ -16,7 +16,9 @@ import pkg_resources
 def get_caldb_file(subpath):
     try:
         # Construct the full path to the data file
+        print(subpath)
         resource_path = os.path.join("CALDB", "aditya-l1", "solexs", "data", "cpf", subpath)
+        print(resource_path)
         response_file_path = pkg_resources.resource_filename("solexs_tools", resource_path)
         print(response_file_path)
         return response_file_path
