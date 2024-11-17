f9efae2183e0e5155b0952682170cff4932bb553
Abhilash Sarwade <as.sarwade@gmail.com>
2024-11-17 22:43:02 +0530

    Modified MANIFEST

--- solexs_tools/__init__.py
+++ solexs_tools/__init__.py
@@ -5,14 +5,15 @@
 # @File Name: __init__.py
 # @Project: solexs_tools
 #
-# @Last Modified time: 2024-11-17 10:25:59 pm
+# @Last Modified time: 2024-11-17 10:42:01 pm
 #####################################################
 
 __version__ = 0.1
 
 import os
+import pkg_resources
 
-def get_caldb_file(subpath):
+def get_caldb_file2(subpath):
     # Construct the full path to the data file
     # print(subpath)
     resource_path = os.path.join("CALDB", "aditya-l1", "solexs", "data", "cpf", subpath)
@@ -21,3 +22,22 @@ def get_caldb_file(subpath):
     response_file_path = os.path.join(basedir_path, resource_path)
     print(response_file_path)
     return response_file_path
+
+import pkg_resources  # For accessing package data
+
+def get_caldb_file(subpath):
+    """
+    Get the absolute path to a file in the CALDB directory.
+
+    Args:
+        subpath (str): Relative path within the CALDB directory (e.g., "response/response_file1.rsp").
+
+    Returns:
+        str: Absolute path to the file.
+    """
+    try:
+        # Construct the full path to the data file
+        resource_path = os.path.join("CALDB", "aditya-l1", "solexs", "data", "cpf", subpath)
+        return pkg_resources.resource_filename("solexs_tools", resource_path)
+    except Exception as e:
+        raise FileNotFoundError(f"File '{subpath}' not found in CALDB: {e}")
