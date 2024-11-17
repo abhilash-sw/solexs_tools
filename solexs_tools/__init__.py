98b18f79fa87ded41c48ee8512939410f6479703
Abhilash Sarwade <as.sarwade@gmail.com>
2024-11-17 22:09:47 +0530

    Test

--- solexs_tools/__init__.py
+++ solexs_tools/__init__.py
@@ -5,18 +5,20 @@
 # @File Name: __init__.py
 # @Project: solexs_tools
 #
-# @Last Modified time: 2024-11-17 09:40:57 pm
+# @Last Modified time: 2024-11-17 10:09:32 pm
 #####################################################
 
 __version__ = 0.1
 
 import os
-import pkg_resources  # For accessing package data
+import pkg_resources
 
 def get_caldb_file(subpath):
     try:
         # Construct the full path to the data file
         resource_path = os.path.join("CALDB", "aditya-l1", "solexs", "data", "cpf", subpath)
-        return pkg_resources.resource_filename("solexs_tools", resource_path)
+        response_file_path = pkg_resources.resource_filename("solexs_tools", resource_path)
+        print(response_file_path)
+        return response_file_path
     except Exception as e:
         raise FileNotFoundError(f"File '{subpath}' not found in CALDB: {e}")
