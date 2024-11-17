9848b0addc8c617171722f15811e1e616709bdaa
Abhilash Sarwade <as.sarwade@gmail.com>
2024-11-17 22:26:31 +0530

    using __file__ to get pahts

--- solexs_tools/__init__.py
+++ solexs_tools/__init__.py
@@ -5,22 +5,19 @@
 # @File Name: __init__.py
 # @Project: solexs_tools
 #
-# @Last Modified time: 2024-11-17 10:20:38 pm
+# @Last Modified time: 2024-11-17 10:25:59 pm
 #####################################################
 
 __version__ = 0.1
 
 import os
-import pkg_resources
 
 def get_caldb_file(subpath):
-    try:
-        # Construct the full path to the data file
-        print(subpath)
-        resource_path = os.path.join("CALDB", "aditya-l1", "solexs", "data", "cpf", subpath)
-        print(resource_path)
-        response_file_path = pkg_resources.resource_filename("solexs_tools", resource_path)
-        print(response_file_path)
-        return response_file_path
-    except Exception as e:
-        raise FileNotFoundError(f"File '{subpath}' not found in CALDB: {e}")
+    # Construct the full path to the data file
+    # print(subpath)
+    resource_path = os.path.join("CALDB", "aditya-l1", "solexs", "data", "cpf", subpath)
+    # print(resource_path)
+    basedir_path = os.path.join(os.path.dirname(__file__), '..')
+    response_file_path = os.path.join(basedir_path, resource_path)
+    print(response_file_path)
+    return response_file_path
