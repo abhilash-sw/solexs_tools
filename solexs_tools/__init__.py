6e3fe4b2c2f7ce9ce9dfb6b30d76976be627d117
abhilash@marvin <sarwade@ursc.gov.in>
2026-01-06 11:54:41 +0000

    Removed __caldb_version__. Issue #26

--- solexs_tools/__init__.py
+++ solexs_tools/__init__.py
@@ -5,8 +5,7 @@
 # @File Name: __init__.py
 # @Project: solexs_tools
 #
-# @Last Modified time: 2026-01-02 05:30:17 pm
+# @Last Modified time: 2026-01-06 05:23:54 pm
 #####################################################
 
 __version__ = 1.2
-__caldb_version__ = 1
